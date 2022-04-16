5486d0b5b88c4407b50d0a6ef10344960332589c
Zain-de94 <71317396+Zain-de94@users.noreply.github.com>
2022-04-16 14:15:14 +0530

    Lesson-2-Displaying_all_three_body_parts

--- app/src/main/java/com/example/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android_me/ui/AndroidMeActivity.java
@@ -1,14 +1,43 @@
 package com.example.android_me.ui;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
 import com.example.android_me.R;
+import com.example.android_me.data.AndroidImageAssets;
 
 public class AndroidMeActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_me);
+
+        BodyPartFragment headFragment = new BodyPartFragment();
+
+        headFragment.setImageIds(AndroidImageAssets.getHeads());
+        headFragment.setListIndex(1);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.beginTransaction()
+                .add(R.id.head_container,headFragment)
+                .commit();
+
+        BodyPartFragment bodyFragment = new BodyPartFragment();
+        bodyFragment.setImageIds(AndroidImageAssets.getBodies());
+        fragmentManager.beginTransaction()
+                .add(R.id.body_container , bodyFragment)
+                .commit();
+
+        BodyPartFragment legFragment = new BodyPartFragment();
+        legFragment.setImageIds(AndroidImageAssets.getLegs());
+        fragmentManager.beginTransaction()
+                .add(R.id.leg_container , legFragment)
+                .commit();
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/android_me/ui/BodyPartFragment.java
@@ -0,0 +1,56 @@
+package com.example.android_me.ui;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.android_me.R;
+import com.example.android_me.data.AndroidImageAssets;
+
+import java.util.List;
+
+public class BodyPartFragment extends Fragment {
+
+    private static final String TAG = "BodyPartFragment";
+
+    private List<Integer> mImageIds;
+    private int mListIndex;
+
+    public BodyPartFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
+
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
+
+        if (mImageIds != null) {
+            imageView.setImageResource(mImageIds.get(mListIndex));
+        } else {
+            Log.v(TAG, "this fragment has a null list of image id's");
+        }
+
+        return rootView;
+
+
+    }
+
+    public void setImageIds(List<Integer> imageIds) {
+        mImageIds = imageIds;
+    }
+
+    public void setListIndex(int index) {
+        mListIndex = index;
+    }
+
+
+}
